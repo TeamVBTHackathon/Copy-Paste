f1cbcd133ec825ecdd9a30a132ab5d25abd0e7c4
gokhanbirkin <gkandth@gmail.com>
2022-02-07 13:32:39 +0300

    main

---
+++ hackathon/android/app/src/main/kotlin/com/example/hackathon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hackathon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hackathon/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
