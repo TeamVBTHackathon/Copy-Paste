a01564b7169156a3337fc26cb9524f2ad6eb085a
bernademir <berna.demir90@gmail.com>
2022-02-12 21:35:12 +0300

    profil view updated

--- hackathon/lib/core/constants/icon/homepage_icon_constants.dart
+++ hackathon/lib/core/constants/icon/homepage_icon_constants.dart
@@ -5,7 +5,7 @@ import '../../init/theme/theme_purple.dart';
 class HomePageIcon {
   static const IconData locationIcon = Icons.location_on;
   static const Icon searchIcon = Icon(Icons.search);
-  static const Icon homePaletIcon = Icon(
+  static Icon homePaletIcon = const Icon(
     Icons.palette,
     color: ThemePurple.darkPurple,
     size: 30,
