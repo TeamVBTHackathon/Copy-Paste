dc85916eeeb97045fa8fc7fc2b72c632ba723ceb
gokhanbirkin <gkandth@gmail.com>
2022-02-12 16:30:44 +0300

    firebase_auth_0.1

--- hackathon/lib/features/authenticate/register/view/register_view.dart
+++ hackathon/lib/features/authenticate/register/view/register_view.dart
@@ -1,19 +1,43 @@
 library register_view;
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:hackathon/core/extension/context_extension.dart';
+import 'package:hackathon/features/authenticate/register/model_view/register_view_model.dart';
+import 'package:hackathon/features/authenticate/register/service/register_service.dart';
 import 'package:hackathon/features/authenticate/widgets/animation_widget.dart';
+import 'package:hackathon/features/authenticate/widgets/custom_text_form_field_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
-part 'parts/register_body.dart';
+part 'parts/register_view_body.dart';
 
-class RegisterView extends StatelessWidget {
+class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
 
   @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        const AnimationWidget(),
-      ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimationWidget(
+            querterTurns: 4,
+          ),
+          SingleChildScrollView(
+              child: Padding(
+            padding: EdgeInsets.only(top: context.height * 0.1),
+            child: RegisterViewBody(),
+          )),
+        ],
+      ),
     );
   }
 }
---
+++ hackathon/lib/features/authenticate/widgets/custom_text_form_field_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon/core/extension/context_extension.dart';
+import 'package:hackathon/core/init/theme/theme_purple.dart';
+
+class CustomTextFormFieldWidget extends StatelessWidget {
+  TextEditingController controller;
+  String labelText;
+  Icon icon;
+  TextInputType? textInputType;
+  void Function()? onEditingComplete;
+  String? errorText;
+  bool? isObsecure;
+  String Function(String?)? validator;
+  bool? isEnabled;
+  CustomTextFormFieldWidget(
+      {Key? key,
+      required this.controller,
+      required this.labelText,
+      required this.icon,
+      this.onEditingComplete,
+      this.errorText,
+      this.isObsecure,
+      this.validator,
+      this.textInputType,
+      this.isEnabled})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: context.width * 0.08, vertical: context.height * 0.009),
+      child: TextFormField(
+        enabled: isEnabled ?? true,
+        controller: controller,
+        obscureText: isObsecure ?? false,
+        validator: validator,
+        onEditingComplete: onEditingComplete,
+        keyboardType: textInputType,
+        decoration: InputDecoration(
+          enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent)),
+          border: InputBorder.none,
+          filled: true,
+          fillColor: Colors.grey.shade100,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.pinkAccent.shade100),
+            borderRadius: BorderRadius.circular(context.height * 0.02),
+          ),
+          labelText: labelText,
+          errorText: errorText,
+          errorStyle: ThemePurple().theme.textTheme.headline5!.copyWith(
+                color: ThemePurple().theme.errorColor,
+              ),
+          prefixIcon: icon,
+        ),
+      ),
+    );
+  }
+}
