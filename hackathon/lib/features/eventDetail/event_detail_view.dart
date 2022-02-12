ad56f225c31d231318284621cad48e60e8d65b59
mesud07 <56825677+mesud07@users.noreply.github.com>
2022-02-12 04:36:02 +0300

    top card düzenlenecek

--- hackathon/lib/features/eventDetail/event_detail_view.dart
+++ hackathon/lib/features/eventDetail/event_detail_view.dart
@@ -38,198 +38,280 @@ class _EventDetailState extends State<EventDetail> {
           child: Column(
            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-        Container(
-          margin: EdgeInsets.only(top: 30,bottom: 30),
-          child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children:[
-      
-      IconButton(icon: const Icon(Icons.arrow_back,color: Colors.black,size: 35,),
-    
-   onPressed: (){},),
-    IconButton(
-      icon:Icon(favorite==false ?Icons.favorite_outline:Icons.favorite,color: redTheme.primaryColor,size: 35,),onPressed: ()=>setState(() {
-        favorite=!favorite;
-      })),
-   ]
-  ),
-        ),
-              //TopContainer starting
-            Container(
-              padding: EdgeInsets.only(left: 15),
-              height: 250,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: redTheme.primaryColor,
-                borderRadius: BorderRadius.circular(10)
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                
-                  children: [
-                    Container(
-                      
-                      width: 400,
-                     // color: Colors.black,
-                      padding: EdgeInsets.only(top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                        Container(
-                          //color: Colors.orange,
-                          width: 200,  
-                                            margin: EdgeInsets.only(
-                                                left: 5,),
-                                            child: Text(
-                                              "Woodly Virtual Event The Creative",
-                                            style:headerStyle,)
-                                          ),
-                                          Container(
-                                             margin: EdgeInsets.only(right: 40),
-                                            child: Icon(Icons.event,size: 30,color: Colors.orange,))
-                                          
-                      ],),
-                    ),
-                   
-                    Row(
-                                children: [
-                                  Icon(Icons.announcement,color: Colors.yellow,size: 25,),
-                                  Text("Hosted by Veli Bacık",style: hostTextStyle,)
-                                ],
-                              )
-        
-                    
-                  ],
-                ),
-            ),
-            //Top Container finished
-        
-           //About Content starting
-           Container(
-             margin: EdgeInsets.only(top: 20),
-             child: Column(children: [
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: [
-                   Text("About this event",style: TextThemeLight.instance!.headline5,),
-                   IconButton(onPressed: (){}, icon: Icon(Icons.comment,color: redTheme.primaryColor,))
-        
-                 ],
-               ),
-        
-               Container(
-                 padding: EdgeInsets.only(right: 10,bottom: 20),
-                 child: Text(aboutsText,style: aboutStyle)),
-        
-             ],),
-           ),
-        
-           //About Content Finished
-        
-        
-          Container(
-                   padding: EdgeInsets.only(left: 5),
-                   child: Text("Next schedules",style: TextThemeLight.instance!.headline5,)),
-        
-        
-           //Next Schadule starting
-        
-           Container(
-             //color: Colors.orange,
-             height: 100,
-             width: MediaQuery.of(context).size.width *3/4,
-             child:Row(
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-        
-                 
-                 Column(
-                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                       children: [
-                       Icon(Icons.date_range),
-                       Text(_eventDate),
-                     ],),
-                     Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                       children: [
-                       Icon(Icons.schedule),
-                       Text(_timeRange),
-                     ],),
-                    
-                   ],
-                 ),
-                  Column(
-                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                      Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                       children: [
-                       Icon(Icons.video_call),
-                       Text(_eventType),
-                     ],),
-                     Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                       children: [
-                       Icon(Icons.schedule),
-                       Text(_duration),
-                     ],)
-                   ],
-                 ),
-                 
-             ],)
-           ),
-        
-          //Scahudele finished
-        
-        
-        
-          //Participant profile starting
-        
-          Row(
-            children: [
-              Container(
-                width: 160,
-                height: 35,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (context,index){
-                return CircleAvatar(
-                  backgroundColor: Colors.grey,
-                );
-              })),
-              Text(" +15 participants",style: TextStyle(fontSize: 15),)
-            ],
-          ),
-        
-           //Participant profile finished
 
-        Container(
-          margin: EdgeInsets.only(top: 20),
-          width: double.infinity,
-          height: 50,
-          child: ElevatedButton(onPressed: (){},
-          style: ElevatedButton.styleFrom(
-            primary: redTheme.primaryColor,
-            shape: RoundedRectangleBorder(
-               borderRadius: BorderRadius.circular(30.0),
-               ),
-          ),
-           child: Text("Join"))),
-           //Join Button starting
+          
+            
+          //ArrowBack and favorite Icon
+          header(),
+
+
+          //Top Content 
+          TopContentWidget(),
+            
+        
+          //About Content 
+          AboutContentWidget(),
+        
+
+          //Next Schadule 
+          SchedulesContentWidget(eventDate: _eventDate, timeRange: _timeRange, eventType: _eventType, duration: _duration),
+        
+        
+          //Participant profile 
+          ParticipantContentWidget(),
+        
+         
+          //Join Button 
+          JoinButton(),
+         
            
         
         
           ],),
         ),
       )
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+  Container header() {
+    return Container(
+        margin: EdgeInsets.only(top: 30,bottom: 30),
+        child: Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children:[
+    
+    IconButton(icon: const Icon(Icons.arrow_back,color: Colors.black,size: 35,),
+  
+ onPressed: (){},),
+  IconButton(
+    icon:Icon(favorite==false ?Icons.favorite_outline:Icons.favorite,color: redTheme.primaryColor,size: 35,),onPressed: ()=>setState(() {
+      favorite=!favorite;
+    })),
+ ]
+),
+      );
+  }
+}
+
+class JoinButton extends StatelessWidget {
+  const JoinButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(onPressed: (){},
+      style: ElevatedButton.styleFrom(
+        primary: redTheme.primaryColor,
+        shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(30.0),
+           ),
+      ),
+       child: Text("Join")));
+  }
+}
+
+class ParticipantContentWidget extends StatelessWidget {
+  const ParticipantContentWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 160,
+          height: 35,
+          child: ListView.builder(
+            shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: 4,
+          itemBuilder: (context,index){
+          return CircleAvatar(
+            backgroundColor: Colors.grey,
+          );
+        })),
+        Text(" +15 participants",style: TextStyle(fontSize: 15),)
+      ],
+    );
+  }
+}
+
+class SchedulesContentWidget extends StatelessWidget {
+  const SchedulesContentWidget({
+    Key? key,
+    required String eventDate,
+    required String timeRange,
+    required String eventType,
+    required String duration,
+  }) : _eventDate = eventDate, _timeRange = timeRange, _eventType = eventType, _duration = duration, super(key: key);
+
+  final String _eventDate;
+  final String _timeRange;
+  final String _eventType;
+  final String _duration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+                 padding: EdgeInsets.only(left: 5),
+                 child: Text("Next schedules",style: TextThemeLight.instance!.headline5,)),
+      
+      Container(
+       //color: Colors.orange,
+       height: 100,
+       width: MediaQuery.of(context).size.width *3/4,
+       child:Row(
+         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+        
+           
+           Column(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                 Icon(Icons.date_range),
+                 Text(_eventDate),
+               ],),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                 Icon(Icons.schedule),
+                 Text(_timeRange),
+               ],),
+              
+             ],
+           ),
+            Column(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+                Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                 Icon(Icons.video_call),
+                 Text(_eventType),
+               ],),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                 Icon(Icons.schedule),
+                 Text(_duration),
+               ],)
+             ],
+           ),
+           
+       ],)
+     ),
+      ],
+    );
+  }
+}
+
+class AboutContentWidget extends StatelessWidget {
+  const AboutContentWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("About this event",style: TextThemeLight.instance!.headline5,),
+            IconButton(onPressed: (){}, icon: Icon(Icons.comment,color: redTheme.primaryColor,))
+        
+          ],
+        ),
+        
+        Container(
+          padding: EdgeInsets.only(right: 10,bottom: 20),
+          child: Text(aboutsText,style: aboutStyle)),
+        
+      ],),
+    );
+  }
+}
+
+class TopContentWidget extends StatelessWidget {
+  const TopContentWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 15),
+      height: 250,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: redTheme.primaryColor,
+        borderRadius: BorderRadius.circular(10)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        
+          children: [
+            Container(
+              
+              width: 400,
+             // color: Colors.black,
+              padding: EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Container(
+                  //color: Colors.orange,
+                  width: 200,  
+                                    margin: EdgeInsets.only(
+                                        left: 5,),
+                                    child: Text(
+                                      "Woodly Virtual Event The Creative",
+                                    style:headerStyle,)
+                                  ),
+                                  Container(
+                                     margin: EdgeInsets.only(right: 40),
+                                    child: Icon(Icons.event,size: 30,color: Colors.orange,))
+                                  
+              ],),
+            ),
+           
+            Row(
+                        children: [
+                          Icon(Icons.announcement,color: Colors.yellow,size: 25,),
+                          Text("Hosted by Veli Bacık",style: hostTextStyle,)
+                        ],
+                      )
+        
+            
+          ],
+        ),
     );
   }
 }
