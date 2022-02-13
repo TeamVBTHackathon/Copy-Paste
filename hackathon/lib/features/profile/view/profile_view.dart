2efbbf3f5fff35acd4e8bf6951d5b30bd58fa08e
gokhanbirkin <gkandth@gmail.com>
2022-02-13 14:54:17 +0300

    commits_branch

--- hackathon/lib/features/profile/view/profile_view.dart
+++ hackathon/lib/features/profile/view/profile_view.dart
@@ -17,7 +17,7 @@ part 'parts/createeventcontent.dart';
 class ProfileView extends StatefulWidget {
   const ProfileView({Key? key}) : super(key: key);
 
-   static const routeName = '/profile';
+  static const routeName = '/profile';
 
   @override
   State<ProfileView> createState() => _ProfileViewState();
@@ -35,53 +35,52 @@ class _ProfileViewState extends State<ProfileView>
 
   @override
   Widget build(BuildContext context) {
-
-    Color _iconColor = ThemePurple.greyOpacityColor;
+    Color _iconColor = ThemePurple.darkPurple;
     return Scaffold(
       appBar: const CustomAppbar(
         headline: ProfileStrings.headline,
       ),
-      body: Column(
-        children: [
-          const Expanded(
-            flex: 4,
-            child: ScrollRowWidget()
-          ),
-           const Expanded(
-            flex: 2,
-            child: CreateEventWidget()
-          ),
-          Expanded(
-            flex: 1,
-            child: TabBar(
-              indicatorColor: ThemePurple.darkPurple,
-              labelColor: ThemePurple.darkPurple,
-              unselectedLabelColor: ThemePurple.blackOpacityColor,
-              controller: _controller,
-              tabs: const [
-                Tab(
-                  text: ProfileStrings.allEvents,
+      body: SingleChildScrollView(
+        child: Container(
+          height: context.height,
+          width: double.infinity,
+          child: Column(
+            children: [
+              const Expanded(flex: 4, child: ScrollRowWidget()),
+              const Expanded(flex: 2, child: CreateEventWidget()),
+              Expanded(
+                flex: 1,
+                child: TabBar(
+                  indicatorColor: ThemePurple.darkPurple,
+                  labelColor: ThemePurple.darkPurple,
+                  unselectedLabelColor: ThemePurple.blackOpacityColor,
+                  controller: _controller,
+                  tabs: const [
+                    Tab(
+                      text: ProfileStrings.allEvents,
+                    ),
+                    Tab(
+                      text: ProfileStrings.savedEvent,
+                    ),
+                  ],
                 ),
-                Tab(
-                  text: ProfileStrings.savedEvent,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 8,
-            child: SizedBox(
-              height: context.height,
-              child: TabBarView(
-                controller: _controller,
-                children: <Widget>[
-                  ListViewWidget(iconColor: _iconColor),
-                  ListViewWidget(iconColor: _iconColor),
-                ],
               ),
-            ),
+              Expanded(
+                flex: 8,
+                child: SizedBox(
+                  height: context.height,
+                  child: TabBarView(
+                    controller: _controller,
+                    children: <Widget>[
+                      ListViewWidget(iconColor: _iconColor),
+                      ListViewWidget(iconColor: _iconColor),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
